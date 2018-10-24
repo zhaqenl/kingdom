a7daab0ce886adbdbd2e8ff57805afdeb46866b7
Raymund Martinez <zhaqenl@gmail.com>
2018-10-24 11:35:06 +0800

    Convert to OOP structure

--- core.py
+++ core.py
@@ -1,27 +1,40 @@
 """Return coordinates of free/open fields and the coordinates of the occupying armies.
 """
 
-def find_alpha(string_grid):
-    matrix = string_grid.split()
-    army_field = dict()
-    for row_index, row in enumerate(matrix):
-        for column_index, column in enumerate(row):
-            if column.isalpha() and not column in army_field:
-                army_field[column] = [(row_index, column_index)]
-            elif column.isalpha() and column in army_field:
-                army_field[column] += [(row_index, column_index)]
-    return army_field
+class KingdomSolver:
 
-def cross_check(coord, matrix):
-    max_row_len, max_column_len = len(matrix[0]) - 1, len(matrix) - 1
-    delta_coords = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-    row_index, column_index = coord
-    set_ = set([(row_index + row, column_index + column) for row, column in delta_coords
-                if row_index + row >= 0 <= column_index + column and
-                row_index + row <= max_column_len and column_index + column <= max_row_len and
-                not matrix[row_index + row][column_index + column] == '#'])
-    set_.update([coord])
-    return set_
+    def __init__(self):
+        self.delta_coords = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+        self.army_field = dict()
+        
+    def map_alpha(self, string_grid):
+        """Return dictionary containing letter and coordinate pairs.
+        """
+        matrix = string_grid.split()
+        
+        for row_index, row in enumerate(matrix):
+            for column_index, column in enumerate(row):
+                if column.isalpha() and not column in self.army_field:
+                    self.army_field[column] = [(row_index, column_index)]
+                elif column.isalpha() and column in self.army_field:
+                    self.army_field[column] += [(row_index, column_index)]
 
-def field_check(coord, matrix):
-    pass
+        return self.army_field
+
+    def cross_check(self, coord, matrix):
+        """Temporary function to be removed later if future function deems this one as obsolete.
+        """
+        max_row_len, max_column_len = len(matrix[0]) - 1, len(matrix) - 1
+        row_index, column_index = coord
+
+        if matrix[row_index][column_index].isalpha():
+            set_ = set([(row_index + row, column_index + column) for row, column in self.delta_coords if
+                        row_index + row >= 0 <= column_index + column and
+                        row_index + row <= max_column_len and column_index + column <= max_row_len
+                        and not matrix[row_index + row][column_index + column] == '#'])
+            set_.add(coord)
+            return set_
+        else:
+            return set()
+
+    
--- tests.py
+++ tests.py
@@ -2,26 +2,28 @@ from nose.tools import *
 import core
 import input_data
 
+solver = core.KingdomSolver()
+
+def coord_match():
+    pass
+
 def test_find_alpha():
-    assert_equal(core.find_alpha(input_data.my_test_grid), {'e': [(0, 1), (2, 1)]})
+    assert_equal(solver.map_alpha(input_data.simple_grid), {'e': [(1, 3)]})
 
 def test_cross_check():
-    assert_equal(core.cross_check((1, 1), input_data.my_test_grid.split()), set([(0, 1), (1, 0),
-                                                                                 (2, 1), (1, 1)]))
+    assert_equal(solver.cross_check((1, 1), input_data.simple_grid.split()), set())
 
 def test_cross_check_zero_out():
-    assert_equal(core.cross_check((0, 0), input_data.my_test_grid.split()), set([(0, 1), (1, 0),
-                                                                                 (0, 0)]))
+    assert_equal(solver.cross_check((0, 0), input_data.simple_grid.split()), set())
 
-def test_cross_check_strict():
-    assert_equal(core.cross_check((2, 1), input_data.my_test_grid.split()), set([(2, 1), (2, 0),
-                                                                                 (1, 1)]))
+def test_cross_check_army():
+    assert_equal(solver.cross_check((1, 3), input_data.simple_grid.split()), set([(1, 3),
+                                                                                (2, 3)]))
 
-def test_cross_check_again():
-    assert_equal(core.cross_check((1, 0), input_data.my_test_grid.split()), set([(1, 0), (1, 1),
-                                                                                 (2, 0)]))
+def test_explode_field_search():
+    assert_equal(solver.explode_field_search((1, 3), input_data.simple_grid.split()),
+                 set([(1, 3), (2, 1), (2, 2), (2, 3), (3, 0), (3, 1), (3, 3)]))
 
-def field_check():
-    assert_equal(core.field_check((0, 0), input_data.simple_grid.split()), set())
-
-
+def snake_crawl():
+    assert_equal(solver.snake_crawl((1, 3), input_data.simple_grid.split()),
+                 set([(1, 3), (2, 1), (2, 2), (2, 3), (3, 0), (3, 1), (3, 3)]))
