d1f08089b3a13119bf816f5ba1987a6a335697ed
Raymund Martinez <zhaqenl@gmail.com>
2018-10-24 20:36:17 +0800

    Add new tests, modify input_data.py, and add new core function

--- core.py
+++ core.py
@@ -6,21 +6,34 @@ class KingdomSolver:
     def __init__(self):
         self.delta_coords = [(0, -1), (-1, 0), (0, 1), (1, 0)]
         self.army_field = dict()
-        self.collected = []
-        self.pivots = []
         
-    def coord_valid(self, coord, matrix):
-        """Return True if coord is not a mountain symbol, False if it is.
+    def coord_field(self, coord, matrix):
+        """Return True if coord is a field symbol, False if it is not.
         """
         max_row = len(matrix) - 1
         max_column = len(matrix[0]) - 1
-        
         row_index = coord[0]
         column_index = coord[1]
+        
         if 0 <= row_index <= max_row and 0 <= column_index <= max_column and \
-        matrix[row_index][column_index] != '#' and not coord in self.collected:
+        matrix[row_index][column_index] == '.':
             return True
+        
         return False
+    
+    def coord_army(self, coord, matrix):
+        """Return True if coord is an army symbol, False if it is not.
+        """
+        max_row = len(matrix) - 1
+        max_column = len(matrix[0]) - 1
+        row_index = coord[0]
+        column_index = coord[1]
+
+        if 0 <= row_index <= max_row and 0 <= column_index <= max_column and \
+        matrix[row_index][column_index].isalpha():
+            return True
+        
+        return False        
 
     def map_alpha(self, string_grid):
         """Return dictionary containing letter and coordinate pairs.
@@ -36,35 +49,30 @@ class KingdomSolver:
 
         return self.army_field
 
-    def cross_check(self, coord, matrix):
-        """Temporary function to be removed later if future function deems this one as obsolete.
-        """
-        max_row_len, max_column_len = len(matrix[0]) - 1, len(matrix) - 1
-        row_index, column_index = coord
-
-        if matrix[row_index][column_index].isalpha():
-            set_ = set([(row_index + row, column_index + column) for row, column in
-                        self.delta_coords if row_index + row >= 0 <= column_index + column and
-                        row_index + row <= max_column_len and column_index + column <= max_row_len
-                        and not matrix[row_index + row][column_index + column] == '#'])
-            set_.add(coord)
-            return set_
-        else:
-            return set()
-
     def flood_fill(self, coord, matrix):
-        """Return the coordinates connected to coord.
+        """Return set of field coordinates and army coordinates connected to coord.
         """
-        self.pivots = [coord]
+        pivots = [coord]
+        collected_field = set()
+        collected_army = set()
         
-        while self.pivots:
-            curr_row, curr_column = self.pivots.pop(0)
-            self.collected += [(curr_row, curr_column)]
+        while pivots:
+            curr_row, curr_column = pivots.pop(0)
+            collected_field.add((curr_row, curr_column))
             for dir_ in self.delta_coords:
-                x, y = dir_[0], dir_[1]
-                new_row, new_column = curr_row + x, curr_column + y
-                if self.coord_valid((new_row, new_column), matrix):
-                    self.collected += [(new_row, new_column)]
-                    self.pivots.append((new_row, new_column))
+                delta_row = dir_[0]
+                delta_column = dir_[1]
+                new_row = curr_row + delta_row
+                new_column = curr_column + delta_column
+                if self.coord_field((new_row, new_column), matrix) and not (new_row, new_column) \
+                in collected_field:
+                    collected_field.add((new_row, new_column))
+                    pivots.append((new_row, new_column))
 
-        return set(self.collected)
+                if self.coord_army((new_row, new_column), matrix) and not (new_row, new_column) \
+                in collected_field:
+                    collected_field.add((new_row, new_column))
+                    collected_army.add((new_row, new_column))
+                    pivots.append((new_row, new_column))
+                    
+        return collected_field, collected_army
--- tests.py
+++ tests.py
@@ -3,29 +3,28 @@ import core
 import input_data
 
 solver = core.KingdomSolver()
-matrix = input_data.simple_grid.split()
+matrix_simple = input_data.simple_grid.split()
+matrix_ebzzry = input_data.ebzzry_grid.split()
 
 def test_find_alpha():
-    assert_equal(solver.map_alpha(input_data.simple_grid), {'e': [(1, 3)]})
+    assert_equal(solver.map_alpha(input_data.simple_grid), {'e': [(1, 3)], 'f': [(3, 1)]})
 
-def test_cross_check():
-    assert_equal(solver.cross_check((1, 1), matrix), set())
+def test_coord_field_true():
+    assert_equal(solver.coord_field((3, 0), matrix_simple), True)
 
-def test_cross_check_zero_out():
-    assert_equal(solver.cross_check((0, 0), matrix), set())
+def test_coord_field_false():
+    assert_equal(solver.coord_field((3, 2), matrix_simple), False)
 
-def test_cross_check_army():
-    assert_equal(solver.cross_check((1, 3), matrix), set([(1, 3), (2, 3)]))
+def test_coord_army_false():
+    assert_equal(solver.coord_army((1, 7), matrix_ebzzry), False)
 
-def test_coord_valid_true():
-    assert_equal(solver.coord_valid((3, 1), matrix), True)
-
-def test_coord_valid_false():
-    assert_equal(solver.coord_valid((3, 2), matrix), False)
+def test_coord_army_true():
+    assert_equal(solver.coord_army((1, 8), matrix_ebzzry), True)
 
 def flood_fill_non_valid():
-    assert_equal(solver.flood_fill((3, 1), matrix), set())
+    assert_equal(solver.flood_fill((3, 1), matrix_simple), set())
 
 def test_flood_fill_valid():
-    assert_equal(solver.flood_fill((1, 3), matrix), set([(1, 3), (2, 3), (3, 3), (2, 2), (2, 1),
-                                                         (3, 1), (3, 0)]))
+    assert_equal(solver.flood_fill((1, 3), matrix_simple), (set([(1, 3), (2, 3), (3, 3), (2, 2),
+                                                                 (2, 1), (3, 1), (3, 0)]),
+                                                            set([(3, 1)])))
